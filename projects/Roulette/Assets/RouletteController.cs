9b7264f75af27e895f6b7a11d8547e96bd7517a5
Pommel Kurama <pommel.pg@gmail.com>
2018-01-21 23:20:56 +0900

    add roulette script.

---
+++ projects/Roulette/Assets/RouletteController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+
+  /** rotational speed */
+  private float rotSpeed = 0;
+
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+    // set rotSpeed when mouse click.
+    if (Input.GetMouseButtonDown(0))
+    {
+      rotSpeed = 10;
+    }
+
+    // revolve roulette.
+    transform.Rotate(0, 0, rotSpeed);
+
+    rotSpeed *= 0.96f;
+  }
+}
