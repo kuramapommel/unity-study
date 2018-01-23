8d0516f7a60d0ae3b60dddc15a8b57abcffe9df6
Pommel Kurama <pommel.pg@gmail.com>
2018-01-23 22:51:18 +0900

    move car by swipe.

---
+++ projects/SwipeCar/Assets/CarController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+
+  float speed = 0;
+  Vector2 startPos;
+  // Use this for initialization
+  void Start()
+  {
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetMouseButtonDown(0))
+    {
+      startPos = Input.mousePosition;
+    }
+    else if (Input.GetMouseButtonUp(0))
+    {
+      var endPos = Input.mousePosition;
+      var swipeLength = endPos.x - startPos.x;
+
+      speed = swipeLength / 500.0f;
+    }
+
+    transform.Translate(speed, 0, 0);
+    speed *= 0.98f;
+  }
+}
