bb8a1f9e1020db35289e44b053c899e0cf76599a
Pommel Kurama <pommel.pg@gmail.com>
2018-01-30 23:46:47 +0900

    When inverting, multiply the enlargement ratio of x axis by -1.

--- projects/ClimbCloud/Assets/PlayerController.cs
+++ projects/ClimbCloud/Assets/PlayerController.cs
@@ -10,13 +10,12 @@ public class PlayerController : MonoBehaviour
   private const float JUMP_FORCE = 680.0f;
   private const float WALK_FORCE = 30.0f;
   private const float MAX_WALK_SPEED = 2.0f;
-  // Use this for initialization
+
   void Start()
   {
     rigid2D = GetComponent<Rigidbody2D>();
   }
 
-  // Update is called once per frame
   void Update()
   {
     if (Input.GetKeyDown(KeyCode.Space))
@@ -24,15 +23,27 @@ public class PlayerController : MonoBehaviour
       rigid2D.AddForce(transform.up * JUMP_FORCE);
     }
 
+    var moveCoefficient = GetMoveCoefficient();
     var speedx = Mathf.Abs(rigid2D.velocity.x);
-
     if (speedx < MAX_WALK_SPEED)
     {
-      rigid2D.AddForce(transform.right * GetInput() * WALK_FORCE);
+      rigid2D.AddForce(transform.right * moveCoefficient * WALK_FORCE);
+    }
+
+    if (moveCoefficient != 0)
+    {
+      // When inverting, multiply the enlargement ratio of x axis by -1
+      var xAxisMultiple = moveCoefficient >= 0 ? 1 : -1;
+      transform.localScale = new Vector3(xAxisMultiple, 1, 1);
     }
   }
 
-  private int GetInput()
+  /**
+	 * get for coefficient of moving.
+	 * 
+	 * @return coefficient of moving. right: 1, left: -1, non: 0;
+	 */
+  private int GetMoveCoefficient()
   {
     if (Input.GetKey(KeyCode.RightArrow)) return 1;
     if (Input.GetKey(KeyCode.LeftArrow)) return -1;
