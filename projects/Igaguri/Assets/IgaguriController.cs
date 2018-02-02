723f07b8047cced77a82f3bd71f5b8ef1157ac14
Pommel Kurama <pommel.pg@gmail.com>
2018-02-02 15:45:51 +0900

    complete igaguri

---
+++ projects/Igaguri/Assets/IgaguriController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriController : MonoBehaviour
+{
+
+  void OnCollisionEnter(Collision other)
+  {
+    GetComponent<Rigidbody>().isKinematic = true;
+    GetComponent<ParticleSystem>().Play();
+  }
+
+  public void Short(Vector3 dir)
+  {
+    GetComponent<Rigidbody>().AddForce(dir);
+  }
+}
---
+++ projects/Igaguri/Assets/IgaguriGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriGenerator : MonoBehaviour
+{
+
+  public GameObject igaguriPrefab;
+  void Update()
+  {
+    if (Input.GetMouseButtonDown(0))
+    {
+      var igaguri = Instantiate(igaguriPrefab) as GameObject;
+
+      var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      var worldDir = ray.direction;
+      igaguri.GetComponent<IgaguriController>().Short(worldDir.normalized * 2000);
+    }
+  }
+}
