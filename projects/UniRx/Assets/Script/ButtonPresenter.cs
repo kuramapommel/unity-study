4b7a6e7a268a430ba04627495168bd3430801c1c
kuramapommel <pommel.pg@gmail.com>
2018-05-18 19:45:40 +0900

    observable

--- projects/UniRx/Assets/Script/ButtonPresenter.cs
+++ projects/UniRx/Assets/Script/ButtonPresenter.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using UniRx;
+using UniRx.Triggers;
+using System;
 
 public class ButtonPresenter : MonoBehaviour
 {
@@ -10,6 +12,8 @@ public class ButtonPresenter : MonoBehaviour
 
     // View
     [SerializeField] private Text m_buttonText;
+
+    private bool m_isA = false;
 
     // Model
     private ButtonModel m_buttonModel;
@@ -28,11 +32,21 @@ public class ButtonPresenter : MonoBehaviour
         m_buttonModel.PushCounter
             .Subscribe(count => m_buttonText.text = count.ToString())
             .AddTo(gameObject);
+
+        // 毎フレーム監視して条件式がtrueのときに処理を実行
+        this.ObserveEveryValueChanged(a => a.m_isA).Subscribe(_ => Debug.Log("m_isAがtrueになりました")).AddTo(gameObject);
     }
     private void Awake()
     {
         InitializeField();
 
         AttachEvent();
+    }
+
+    private void Start()
+    {
+        this.FixedUpdateAsObservable() // 毎フレームごとに
+            .Where(_ => m_buttonModel.PushCounter.Value >= 10) // 10以上かのチェックを行い
+            .Subscribe(_ => Destroy(m_button)); // 10以上ならDestroy
     }
 }
